014d81598be97dcfe58345808cc206e6f95983a4
shakibchakladar <shakibchakladar@gmail.com>
2024-11-28 10:23:54 +0600

    Homepage updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,15 @@ import Review from "../components /Review";
 import WhySpecial from "../components /WhySpecial";
 
 const Home = () => {
-    return (
-        <div>
-            {/* <h2>This is Home</h2> */}
-            <Banner></Banner>
-            <Addv></Addv>
-            <Review></Review>
-            <WhySpecial></WhySpecial>
-            <OurAgent></OurAgent>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <Addv></Addv>
+      <Review></Review>
+      <WhySpecial></WhySpecial>
+      <OurAgent></OurAgent>
+    </div>
+  );
 };
 
 export default Home;
